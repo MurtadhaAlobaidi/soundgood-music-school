579cd9a996df468455000ed42bcc6ff88cda777c
Murtadha Alobaidi <69900487+mhaao@users.noreply.github.com>
2022-01-07 12:38:19 +0100

    Add files via upload

--- view.sql
+++ view.sql
@@ -1,16 +1,7 @@
-CREATE VIEW all_lessones AS
-    SELECT 
-        EXTRACT(YEAR FROM lesson.time) AS year,
-        EXTRACT(MONTH FROM lesson.time) AS month,
-        COUNT(lesson.type_of_lesson) AS numbre_of_lesson
-    FROM lesson
-    GROUP BY (year, month)
-    ORDER BY MONTH;
-
-
-CREATE VIEW lessones_statistics AS
+-- CREATE VIEW lessones_statistics AS
     SELECT 
         EXTRACT(MONTH FROM lesson.time) AS month,
+        EXTRACT(YEAR FROM lesson.time) AS YEAR,
         COUNT(ensemble.type_of_lesson)  AS ensemble,
         COUNT(group_lesson.type_of_lesson)  AS group,
         COUNT(individual_lesson.type_of_lesson)  AS individual
@@ -18,48 +9,35 @@ CREATE VIEW lessones_statistics AS
     FULL OUTER JOIN ensemble ON  lesson.lesson_id=ensemble.lesson_id
     FULL OUTER JOIN group_lesson ON  lesson.lesson_id=group_lesson.lesson_id
     FULL OUTER JOIN individual_lesson ON  lesson.lesson_id=individual_lesson.lesson_id
-    GROUP BY month
+    GROUP BY (month,year)
     ORDER BY  MONTH;
---------------------------------------------------------------------------------------------------------
-CREATE VIEW avarage_lessons AS
+
+
+-- CREATE VIEW avarage_lessons AS
     SELECT 
         EXTRACT(YEAR FROM lesson.time) AS year,
-        COUNT(lesson.type_of_lesson) AS numbre_of_lesson,
-        CAST(COUNT(*) AS NUMERIC)/12 AS avarage_lessons
+        COUNT(lesson.type_of_lesson) AS total_num_of_lessons,
+        CAST(COUNT(individual_lesson.type_of_lesson) AS NUMERIC)/12 AS average_num_of_individual_per_month,
+        CAST(COUNT(group_lesson.type_of_lesson) AS NUMERIC)/12 AS average_num_of_group_per_month,
+        CAST(COUNT(ensemble.type_of_lesson) AS NUMERIC)/12 AS average_num_of_ensemble_per_month
     FROM lesson
+    FULL OUTER JOIN ensemble ON  lesson.lesson_id=ensemble.lesson_id
+    FULL OUTER JOIN group_lesson ON  lesson.lesson_id=group_lesson.lesson_id
+    FULL OUTER JOIN individual_lesson ON  lesson.lesson_id=individual_lesson.lesson_id
     GROUP BY (year);
---------------------------------------------------------------------------------------------------------
-
-CREATE VIEW instructor_statistics AS
-    SELECT 
-        first_name || ' ' || last_name AS Instructor_Name,
-        EXTRACT(MONTH FROM lesson.time) AS month,
-        lesson.type_of_lesson
-    FROM lesson
-    INNER JOIN instructor ON lesson.employment_id=instructor.employment_id
-    INNER JOIN person ON instructor.person_id=person.person_id
-    ORDER BY  MONTH;
 
 
-CREATE VIEW instructors_work_too_much AS
-    SELECT 
-        EXTRACT(MONTH FROM le.time) AS month,
-        COUNT(le.employment_id)  AS number_of_instructors_works_much
-    FROM person AS pe 
-    INNER JOIN instructor AS inj 
-     ON pe.person_id=inj.person_id
-    INNER JOIN lesson   AS le
-    ON inj.employment_id=le.employment_id
-    GROUP BY month
-    HAVING (COUNT(le.employment_id)>1)
-    ORDER BY  MONTH;  
-
-
-CREATE VIEW instructors_total_lesson AS
+-- CREATE VIEW instructor_statistics AS
     SELECT  
-        first_name || ' ' || last_name AS Instructor_Name ,
-        COUNT(lesson.lesson_id) AS total_lessons
+        first_name || ' ' || last_name AS Instructor_Name,
+        COUNT(lesson.lesson_id) AS total_lessons,
+        EXTRACT(MONTH FROM lesson.time) AS month,
+        CASE
+            WHEN COUNT(lesson.lesson_id) = 1 THEN 'False'
+            WHEN COUNT(lesson.lesson_id) > 1 THEN 'True'
+        END AS Overtime
     FROM public.lesson
     INNER JOIN instructor ON instructor.employment_id=lesson.employment_id
     INNER JOIN person ON instructor.person_id=person.person_id
-    GROUP BY Instructor_Name;
+    GROUP BY (Instructor_Name,MONTH) 
+    ORDER BY MONTH;
